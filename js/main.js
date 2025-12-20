bcccf3a10de22ea4956ad6bb27f6542f1f53df65
JanKennethGerona <jkdgerona@addu.edu.ph>
2025-12-20 09:44:54 +0800

    header fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
 // Load header from external file
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header-placeholder').innerHTML = data;
-    })
-    .catch(error => console.error('Error loading header:', error));
